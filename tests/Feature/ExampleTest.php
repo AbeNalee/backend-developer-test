e48d7509f41f262ddd0117dff94be05e691fa139
abenalee <abrahamaguvasu@gmail.com>
2023-11-02 02:45:56 +0300

    http test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -6,6 +6,7 @@ use App\Models\User;
 // use Illuminate\Foundation\Testing\RefreshDatabase;
 use Database\Seeders\DatabaseSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
@@ -22,5 +23,19 @@ class ExampleTest extends TestCase
         $response = $this->get("/users/{$user->id}/achievements");
 
         $response->assertStatus(200);
+    }
+
+    public function testApplicationResponseForNewUserIsCorrectFormat()
+    {
+        $user = User::factory()->create();
+        $this->seed(DatabaseSeeder::class);
+
+        $response = $this->get("/users/{$user->id}/achievements");
+
+        $this->assertTrue(empty($response['unlocked_achievements']));
+        $response->assertJson(fn (AssertableJson $json) =>
+            $json->hasAll(['unlocked_achievements', 'next_available_achievements',
+                'current_badge', 'next_badge', 'remaining_to_unlock_next_badge'])
+        );
     }
 }
